40032d0264e5d027a17b64d78b5c50d728cf686c
Rafael <rafaelcdmm@gmail.com>
2016-06-07 14:17:31 -0300

    Mudanças diversas em páginas Docente
    Mudança nos títulos das páginas, redimensionamento de algumas tabelas,
    adição de ícones, função hover em grades.js, etc.

--- assets/js/docente/grades.js
+++ assets/js/docente/grades.js
@@ -1,10 +1,17 @@
+var popoverTexts =
+{
+		"fa fa-question-circle-o fa-lg":" Indefinido! ",
+		"fa fa-thumbs-o-down fa-lg":" Reprovado! ",
+		"fa fa-thumbs-o-up fa-lg":" Aprovado! "
+}
+
 $(document).ready(function(){
+
+	$('.fa').popover();
 
 	$('.form-control').focusout(function(){
 		var nota = $(this).val();
-		console.log("nota: " + nota);
 		var id = $(this).attr('id').substr(4);
-		console.log("id: " + id);
 		if(nota < 6){
 			$('#resultado' + id).html
 			(
@@ -17,6 +24,17 @@ $(document).ready(function(){
 			"<i class='fa fa-thumbs-o-up fa-lg' style='color:green' aria-hidden='true'></i>"
 			);
 		}
+	});
+
+	$('#gradesTable').delegate('.fa', 'mouseenter mouseleave', function (event)
+	{
+		if (event.type == 'mouseenter') {
+			console.log("hover");
+			$(this).attr('data-content',popoverTexts[$(this).attr('class')]);
+			$(this).popover('show');
+    } else {
+			$(this).popover('hide');
+    }
 	});
 
 });
