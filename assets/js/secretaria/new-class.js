49e6e2ea3c2ddb7a5d4afef5d4ad3e8cc4712475
Igor Moura Brandão <igor-brandao@outlook.com>
2016-06-07 17:59:01 -0300

    Adicionando features para criação de turmas

--- assets/js/secretaria/new-class.js
+++ assets/js/secretaria/new-class.js
@@ -1,22 +1,55 @@
+var totalStudents;
+var totalSubjects;
+var currentStep = 1;
+
 $(document).ready(function () {
     //Initializa as tooltips
     $('.nav-tabs > li a[title]').tooltip();
 
     //Move o wizard
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
         var $target = $(e.target);
-
         if ($target.parent().hasClass('disabled')) {
             return false;
+        }
+        else{
+            currentStep = $(this).attr('href').substr(5);
+            console.log("currentStep changed by wizard: " + currentStep);
         }
     });
 
     $(".next-step").click(function (e) {
 
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
+        if(canProceed()){
+            var $active = $('.wizard .nav-tabs li.active');
+            $active.next().removeClass('disabled');
+            nextTab($active);
+
+            if(currentStep == 2){
+                console.log("$('#totalSubjects').val(): " + $('#totalSubjects').val());
+                console.log("$('#totalSubjects').attr('max'): " + $('#totalSubjects').attr('max'));
+                totalSubjects = normalize($('#totalSubjects').val(), $('#totalSubjects').attr('max'));
+                console.log("totalSubjects: " + totalSubjects);
+                console.log("building step 2 div.");
+                buildStep2Div();
+                console.log("finished building step 2 div.");
+            }
+            else if(currentStep == 3){
+                console.log("$('#totalStudents').val(): " + $('#totalStudents').val());
+                console.log("$('#totalStudents').attr('max'): " + $('#totalStudents').attr('max'));
+                totalStudents = normalize($('#totalStudents').val(), $('#totalStudents').attr('max'));
+                console.log("totalStudents: " + totalStudents);
+                console.log("building step 3 div.");
+                buildStep3Div();
+                console.log("finished building step 3 div.");
+            }
+            else if(currentStep == 4){
+                buildStep4Div();
+            }
+        }
+        else{
+            showAlert();
+        }
 
     });
     $(".prev-step").click(function (e) {
@@ -25,6 +58,7 @@ $(document).ready(function () {
         prevTab($active);
 
     });
+
 });
 
 function nextTab(elem) {
@@ -32,4 +66,147 @@ function nextTab(elem) {
 }
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+function canProceed(){
+
+    var $inputs = $('#step' + currentStep + ' input');
+
+    for (var i = 0, len = $inputs.length; i < len; i++) {
+        if ( !$inputs[i].value ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function normalize(value, max){
+    console.log("value: " + value);
+    console.log("max: " + max);
+    if(value > max){
+        return max;
+    }
+    else if(value < 0){
+        return 1;
+    }
+    else{
+        return value;
+    }
+}
+
+function getRemainingItems(remaining){
+    var counter = 0;
+    while(remaining - counter !== 0){
+        counter++;
+    }
+    return counter;
+}
+
+function buildStep2Div() {
+
+    var divContent = "";
+    var loopCounter = 1;
+    var pageLineItems = 3;
+    var iterationSubjects;
+    var remainingSubjects = totalSubjects;
+
+    while (remainingSubjects > 0) {
+
+        if (remainingSubjects - pageLineItems >= 0) {
+            remainingSubjects -= pageLineItems;
+            iterationSubjects = pageLineItems;
+        }
+        else {
+            iterationSubjects = getRemainingItems(remainingSubjects);
+            remainingSubjects = 0;
+        }
+
+        divContent += "<div class='row'>";
+
+        for(i = 0; i < iterationSubjects; i++){
+            divContent +=
+                "<span class='col-lg-2 pull-left'>" +
+    			    "<label for='Subject'>Disciplina "+(loopCounter+i)+"</label>" +
+        		"</span>" +
+                "<span class='col-lg-2 pull-left'>" +
+        			"<label for='Teacher'>Professor "+(loopCounter+i)+"</label>" +
+        		"</span>";
+        }
+
+        divContent += "</div><div class='row'>";
+
+        for(i = 0; i < iterationSubjects; i++){
+            divContent +=
+                "<span class='col-lg-2'>" +
+        			"<input type='text' class='form-control' id='subject"+(loopCounter+i)+"'>" +
+        		"</span>" +
+        		"<span class='col-lg-2'>" +
+        			"<input type='number' placeholder='Matrícula' class='form-control' id='teacher"+(loopCounter+i)+"'>" +
+        		"</span>";
+        }
+
+        divContent += "</div><br>";
+        loopCounter += pageLineItems;
+
+    }
+    $("#step2div").html(divContent);
+}
+
+function buildStep3Div(){
+
+    var divContent = "";
+    var loopCounter = 1;
+    var pageLineItems = 6;
+    var iterationStudents;
+    var remainingStudents = totalStudents;
+
+    while (remainingStudents > 0) {
+
+        if (remainingStudents - pageLineItems >= 0) {
+            remainingStudents -= pageLineItems;
+            iterationStudents = pageLineItems;
+        }
+        else {
+            iterationStudents = getRemainingItems(remainingStudents);
+            remainingStudents = 0;
+        }
+
+        divContent += "<div class='row'>";
+
+        for(i = 0; i < iterationStudents; i++){
+            divContent +=
+                "<span class='col-lg-2 pull-left'>" +
+                    "<label for='Student'>Aluno "+(loopCounter+i)+"</label>" +
+                "</span>";
+        }
+
+        divContent += "</div><div class='row'>";
+
+        for(i = 0; i < iterationStudents; i++){
+            divContent +=
+                "<span class='col-lg-2'>" +
+                    "<input type='number' placeholder='Matrícula' class='form-control' id='teacher"+(loopCounter+i)+"'>" +
+                "</span>";
+        }
+
+        divContent += "</div><br>";
+        loopCounter += pageLineItems;
+
+    }
+
+    $("#step3div").html(divContent);
+}
+
+function buildStep4Div(){
+    //revisão
+}
+
+function showAlert () {
+    $('#invalidForm').html(
+        "<div id='invalidAlert' class='alert alert-dismissible alert-warning' role='alert'><button type='button' class='close' data-dismiss='alert'aria-label='Close'><span aria-hidden='true'>&times;</span></button>Certifique-se de preencher todos os campos antes de prosseguir.</div>"
+    );
+	$("#invalidAlert").fadeTo(3000, 500).slideUp(500, function(){
+   		$("#invalidAlert").alert('close');
+	});
+    userType = "";
 }
--- assets/js/secretaria/new-user.js
+++ assets/js/secretaria/new-user.js
@@ -8,57 +8,47 @@ function switchForm (formValue) {
 function commonForms(){
 	return "" +
 	"<div class='row'>" +
-		"<span class='form-group'>" +
-			"<span class='col-lg-2 text-center'>" +
-				"<label for='Email'>Email</label>" +
-			"</span>" +
-			"<span class='col-lg-6'>" +
-				"<input type='email' class='form-control' id='email'>" +
-			"</span>" +
+		"<span class='col-lg-2 text-center'>" +
+			"<label for='Email'>Email</label>" +
+		"</span>" +
+		"<span class='col-lg-6'>" +
+			"<input type='email' class='form-control' id='email'>" +
 		"</span>" +
 	"</div>" +
 	"<br>" +
 	"<div class='row'>" +
-		"<span class='form-group'>" +
-			"<span class='col-lg-2 text-center'>" +
-				"<label for='Name' class=''>Nome Completo</label>" +
-			"</span>" +
-			"<span class='col-lg-6'>" +
-				"<input type='text' class='form-control' id='name'>" +
-			"</span>" +
+		"<span class='col-lg-2 text-center'>" +
+			"<label for='Name' class=''>Nome Completo</label>" +
+		"</span>" +
+		"<span class='col-lg-6'>" +
+			"<input type='text' class='form-control' id='name'>" +
 		"</span>" +
 	"</div>" +
 	"<br>" +
 	"<div class='row'>" +
-		"<span class='form-group'>" +
-			"<span class='col-lg-2 text-center'>" +
-				"<label for='Age' class=''>Idade</label>" +
-			"</span>" +
-			"<span class='col-lg-6'>" +
-				"<input type='number' class='form-control' id='age'>" +
-			"</span>" +
+		"<span class='col-lg-2 text-center'>" +
+			"<label for='Age' class=''>Idade</label>" +
+		"</span>" +
+		"<span class='col-lg-6'>" +
+			"<input type='number' class='form-control' id='age'>" +
 		"</span>" +
 	"</div>" +
 	"<br>" +
 	"<div class='row'>" +
-		"<span class='form-group'>" +
-			"<span class='col-lg-2 text-center'>" +
-				"<label for='Adress'>Endereço</label>" +
-			"</span>" +
-			"<span class='col-lg-6'>" +
-				"<input type='text' class='form-control' id='adress'>" +
-			"</span>" +
+		"<span class='col-lg-2 text-center'>" +
+			"<label for='Adress'>Endereço</label>" +
+		"</span>" +
+		"<span class='col-lg-6'>" +
+			"<input type='text' class='form-control' id='adress'>" +
 		"</span>" +
 	"</div>" +
 	"<br>" +
 	"<div class='row'>" +
-		"<span class='form-group'>" +
-			"<span class='col-lg-2 text-center'>" +
-				"<label for='Phone' class=''>Telefone</label>" +
-			"</span>" +
-			"<span class='col-lg-6'>" +
-				"<input type='text' class='form-control' id='phone'>" +
-			"</span>" +
+		"<span class='col-lg-2 text-center'>" +
+			"<label for='Phone' class=''>Telefone</label>" +
+		"</span>" +
+		"<span class='col-lg-6'>" +
+			"<input type='text' class='form-control' id='phone'>" +
 		"</span>" +
 	"</div>" +
 	"<br>";
@@ -71,48 +61,40 @@ function specificForms(formValue){
 	else if ( formValue === "Professor" ) {
 		return "" +
 		"<div class='row'>" +
-			"<span class='form-group'>" +
-				"<span class='col-lg-2 text-center'>" +
-					"<label for='Areas' class=''>Areas de Conhecimento</label>" +
-				"</span>" +
-				"<span class='col-lg-6'>" +
-					"<input type='text' class='form-control' id='areas'>" +
-				"</span>" +
+			"<span class='col-lg-2 text-center'>" +
+				"<label for='Areas' class=''>Areas de Conhecimento</label>" +
+			"</span>" +
+			"<span class='col-lg-6'>" +
+				"<input type='text' class='form-control' id='areas'>" +
 			"</span>" +
 		"</div>";
 	}
 	else if ( formValue === "Responsavel" ) {
 		return "" +
 		"<div class='row'>" +
-			"<span class='form-group'>" +
-				"<span class='col-lg-2 text-center'>" +
-					"<label for='entes' class=''>Matrícula do(s) Ente(s)</label>" +
-				"</span>" +
-				"<span class='col-lg-6'>" +
-					"<input type='text' class='form-control' id='entes'>" +
-				"</span>" +
+			"<span class='col-lg-2 text-center'>" +
+				"<label for='entes' class=''>Matrícula do(s) Ente(s)</label>" +
+			"</span>" +
+			"<span class='col-lg-6'>" +
+				"<input type='text' class='form-control' id='entes'>" +
 			"</span>" +
 		"</div>" +
 		"<br>" +
 		"<div class='row'>" +
-			"<span class='form-group'>" +
-				"<span class='col-lg-2 text-center'>" +
-					"<label for='AP' class=''>Atuação Profissional</label>" +
-				"</span>" +
-				"<span class='col-lg-6'>" +
-					"<input type='text' class='form-control' id='ap'>" +
-				"</span>" +
+			"<span class='col-lg-2 text-center'>" +
+				"<label for='AP' class=''>Atuação Profissional</label>" +
+			"</span>" +
+			"<span class='col-lg-6'>" +
+				"<input type='text' class='form-control' id='ap'>" +
 			"</span>" +
 		"</div>" +
 		"<br>" +
 		"<div class='row'>" +
-			"<span class='form-group'>" +
-				"<span class='col-lg-2 text-center'>" +
-					"<label for='OP' class=''>Organização Profissional</label>" +
-				"</span>" +
-				"<span class='col-lg-6'>" +
-					"<input type='text' class='form-control' id='op'>" +
-				"</span>" +
+			"<span class='col-lg-2 text-center'>" +
+				"<label for='OP' class=''>Organização Profissional</label>" +
+			"</span>" +
+			"<span class='col-lg-6'>" +
+				"<input type='text' class='form-control' id='op'>" +
 			"</span>" +
 		"</div>" +
 		"<br>";
